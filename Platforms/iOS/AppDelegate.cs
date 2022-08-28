e4b14fbacc351864c7a4f87bfe2d1fd1357ea645
Om Panchal <om.panchal@blackhawk.io>
2022-08-28 16:05:57 +1200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeatherDesign;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
